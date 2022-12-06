d6d11d7e49174c337dca7595e60b6c986b3fabee
Ana Perrone <anaclara.perrone@gmail.com>
2022-12-05 18:27:01 -0700

    comments

--- Email.java
+++ Email.java
@@ -30,16 +30,21 @@ public class Email {
         //account we created specifically for the project
     }
 
-    //method called from database with the email of client, content of the email to send and subject 
+    /*
+    * Public method called from registered user and database with email of recipient, subject and content of email
+    * message to send
+    */
     public void sendEmail(String to, String content, String subject) {
 
         try {
             Properties properties = new Properties();
+            //necessary smtp configurations
             properties.put("mail.smtp.auth", true);
             properties.put("mail.smtp.starttls.enable", true);
             properties.put("mail.smtp.host", "smtp.gmail.com");
             properties.put("mail.smtp.port", 587);
 
+            //creating authenticator with designated device password
             Session session = Session.getInstance(properties, new Authenticator() {
                 @Override 
                 protected PasswordAuthentication getPasswordAuthentication() {
@@ -47,16 +52,19 @@ public class Email {
                 }
             });
 
+            //creating message with session configs
             Message message = new MimeMessage(session);
-
+            //creating sender internet address
             message.setFrom(new InternetAddress(from));
-
+            //adding the subject line to the message given from Registered User
             message.setSubject(subject);
+            //adding message content
             message.setContent(content, "text/plain");
-
+            //creating recipient internet address
             Address addressTo = new InternetAddress(to);
+            //setting address as recipient
             message.setRecipient(Message.RecipientType.TO, addressTo);
-
+            //sending email
             Transport.send(message);
 
         } catch(Exception e) {
